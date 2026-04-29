96bb57653c09381b621855b6cd1828bfe2ea7f9a
Alpha <mutethiaamstrong@gmail.com>
2026-04-29 13:58:35 +0300

    Switch to SMTP Port 465 SSL for better delivery

--- backend/app.py
+++ backend/app.py
@@ -27,8 +27,9 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'kweli_secret_123')
 
 # ----------------- MAIL SETUP -----------------
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME', 'amstrongmutethia@gmail.com')
 app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD', 'elfz ayzb lxld pukg')
 app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_USERNAME', 'amstrongmutethia@gmail.com')
